f5906386f8e67600da608008f4083c06b2218b6e
gabrielibagon <gabriel.ibagon@gmail.com>
2021-02-14 13:02:37 -0500

    relaxed python requirements to >=3.6 for use in colab

--- setup.py
+++ setup.py
@@ -17,5 +17,5 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/vistry/ssd_keras",
     packages=setuptools.find_packages(),
-    python_requires='>=3.7',
+    python_requires='>=3.6',
 )
